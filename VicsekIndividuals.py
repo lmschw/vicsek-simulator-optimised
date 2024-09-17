0f199d86ea1e8e4b734c7332a641191a122da351
Lilly Schwarzenbach <lillyschwarzenbach@gmail.com>
2024-09-17 17:29:44 +0200

    add initial implementation of neighbour selection for NEAREST

--- VicsekIndividuals.py
+++ VicsekIndividuals.py
@@ -3,6 +3,9 @@ import pandas as pd
 import numpy as np
 import random
 
+from EnumNeighbourSelectionMechanism import NeighbourSelectionMechanism
+from EnumSwitchType import SwitchType
+
 def generateNoise():
     return np.random.normal(scale=noise, size=(n, len(domainSize)))
 
@@ -13,18 +16,60 @@ def calculateMeanOrientations(orientations, neighbours):
 def normalizeOrientations(orientations):
     return orientations/(np.sqrt(np.sum(orientations**2,axis=1))[:,np.newaxis])
 
-def getNeighbours(positions):
+def getPositionDifferences(positions):
     rij=positions[:,np.newaxis,:]-positions
     #rij=rij[~np.eye(rij.shape[0],dtype=bool),:].reshape(rij.shape[0],rij.shape[0]-1,-1) #remove i<>i interaction
         
     rij = rij - domainSize*np.rint(rij/domainSize) #minimum image convention
 
     rij2 = np.sum(rij**2,axis=2)
+    return rij2
+
+def getNeighbours(positions):
+    rij2 = getPositionDifferences(positions)
 
     neighbours = (rij2 <= radius**2)
+    np.fill_diagonal(neighbours, False)
     return neighbours
 
-def computeNewOrientation(nsm, k, neighbours, positions, orientations):
+def pickPositionNeighbours(k, positions, neighbours, isMin=True):
+    posDiff = getPositionDifferences(positions)
+    if isMin == True:
+        #neighbourDiffs = np.where(neighbours == True, posDiff, maxSq)
+        fillValue = maxSq
+    else:
+        #neighbourDiffs = np.where(neighbours == True, posDiff, minSq)
+        fillValue = minSq
+
+    #a = np.sort(posDiff, axis=1)
+    #mask = neighbours.nonzero()
+    #a = np.argsort(neighbourDiffs, axis=1)
+    diagonalIndices = np.diag_indices(n)[0]
+    maskedArray = np.ma.MaskedArray(posDiff, mask=neighbours==False, fill_value=fillValue)
+    candidates = maskedArray.argsort(axis=1)[:, :k]
+    pickedDistances = np.take_along_axis(posDiff, candidates, axis=1)
+    # TODO: filter on actual neighbours, e.g. by replacing indices that aren't neighbours by the diagonal index (the agent's own index)
+    print("posdiff")
+    print(posDiff)
+    print("picked")
+    print("candidates")
+    return candidates
+
+def computeNewOrientation(nsm, k, neighbours, positions, orientations, vals):
+
+    """
+    match switchType:
+        case SwitchType.NEIGHBOUR_SELECTION_MODE:
+            valsDf = pd.DataFrame(vals)
+            valsDf["val"] = valsDf["val"].case_when([(, valsB),
+                                (((valsDf["localOrder"] <= threshold) & (valsDf["previousLocalOrder"] >= threshold)), valsA),
+            ])
+    """
+    match nsm:
+        case NeighbourSelectionMechanism.NEAREST:
+            pickedNeighbours = pickPositionNeighbours(k, positions, neighbours, True)
+
+
     orientations = calculateMeanOrientations(orientations, neighbours)
     orientations = normalizeOrientations(orientations+generateNoise())
     return orientations
@@ -62,8 +107,11 @@ density = 0.09
 domainSize = (25, 25)
 n = 56
 
+maxSq = domainSize[0] * domainSize[1] + 1
+minSq = -1
+
 radius = 20
-n = 3
+n = 5
 
 indices = []
 for i in range(n):
@@ -71,6 +119,8 @@ for i in range(n):
 
 valsA = tf.zeros([n])
 valsB = tf.ones([n])
+
+
 #indices = tf.constant(indices)
 
 d1pos = [10, 10]
@@ -79,6 +129,10 @@ d2pos = [15, 15]
 d2ori = [-0.5, -10]
 d3pos = [20, 20]
 d3ori = [0.2, 0.2]
+d4pos = [5, 5]
+d4ori = [-0.2, 20]
+d5pos = [12, 12]
+d5ori = [7, 4]
 
 """
 d1ori = [1,1]
@@ -90,12 +144,12 @@ d2ori = [0.38252475, 0.37564835]
 d3ori = [0.38252475, 0.37564835]
 """
 
-vals = tf.constant([0, 1, 1])
+vals = tf.constant([0, 1, 1, 0, 0])
 
 vals = pd.DataFrame(vals, columns=['val'])
 
-positions = tf.constant([d1pos, d2pos, d3pos])
-orientations = tf.constant([d1ori, d2ori, d3ori])
+positions = tf.constant([d1pos, d2pos, d3pos, d4pos, d5pos])
+orientations = tf.constant([d1ori, d2ori, d3ori, d4ori, d5ori])
 orientations = normalizeOrientations(orientations+generateNoise())
 
 localOrdersHistory = []
@@ -123,6 +177,6 @@ for t in range(20):
     vals = getDecisions(t, 10, localOrders, localOrdersHistory, vals)
     print("vals:")
     print(vals)
-    orientations = computeNewOrientation("N", 1, neighbours, positions, orientations)
+    orientations = computeNewOrientation(NeighbourSelectionMechanism.NEAREST, 1, neighbours, positions, orientations, vals)
     print("orientations:")
     print(orientations)
