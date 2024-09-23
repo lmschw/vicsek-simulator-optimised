2fbc759ed49dfc99d16bbed57f465d199d22b929
lmschw <lillyschwarzenbach@gmail.com>
2024-09-23 10:48:13 +0200

    remove RANDOM

--- EnumNeighbourSelectionMechanism.py
+++ EnumNeighbourSelectionMechanism.py
@@ -4,7 +4,6 @@ from enum import Enum
 Contains all the different ways of selecting a subset of neighbours from all possible neighbours.
 """
 class NeighbourSelectionMechanism(str, Enum):
-    RANDOM = "R",
     NEAREST = "N",
     FARTHEST = "F",
     LEAST_ORIENTATION_DIFFERENCE = "LOD",
