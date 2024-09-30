ed2d20dc393fe1391736fee46a6486183866dd3c
lmschw <lillyschwarzenbach@gmail.com>
2024-09-30 10:45:25 +0200

    update test.py

--- test.py
+++ test.py
@@ -1,11 +1,45 @@
-import tensorflow as tf
+import ServicePreparation
+import ServiceMetric
+import ServiceVicsekHelper
+import ServiceOrientations
+import ServiceSavedModel
+
 import numpy as np
 
-rank_0_tensor = tf.constant(4)
-a1 = [[1, 1], [1.1, 1.1], [1, 0]]
-a2 = [[2, 2], [2.2, 2.2], [0, 1]]
 
-t = tf.constant([a1, a2])
 
-print("actual print:")
-print(t)
+#print(ServicePreparation.getNumberOfParticlesForConstantDensity(0.1, (100, 100)))
+#print(ServicePreparation.getDensity((100, 100), 100))
+#print(ServicePreparation.getDomainSizeForConstantDensity(0.01, 5))
+
+switchValues = [5,1]
+domainSize = (100, 100)
+n = 10
+radius = 20
+
+
+"""
+
+positions = domainSize*np.random.rand(n,len(domainSize))
+orientations = ServiceOrientations.normalizeOrientations(np.random.rand(n, len(domainSize))-0.5)
+
+orientations[1] = orientations[8]
+orientations[3] = orientations[8]
+orientations[7] = orientations[1]
+orientations[0] = orientations[2]
+
+
+
+initialState = ServicePreparation.createOrderedInitialDistributionEquidistancedIndividual(switchValues[0], domainSize, n, angleX=0.5, angleY=0.5)
+positions = initialState[0]
+orientations = initialState[1]
+
+
+print(ServiceMetric.findClustersWithRadius(positions, orientations, domainSize, radius, threshold=0.01))
+"""
+
+modelParams, simulationData = ServiceSavedModel.loadModel("test.json")
+time, positions, orientations = simulationData
+
+for t in [0, 1000, 2000, 3000]:
+    print(ServiceMetric.findClustersWithRadius(positions[t], orientations[t], domainSize, radius, threshold=0.01))
