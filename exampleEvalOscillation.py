002c1f68877a1545153ac1f9f8eb1a43fe39c291
Lilly Schwarzenbach <lillyschwarzenbach@gmail.com>
2025-03-27 13:54:32 +0100

    remove oscillation code

--- exampleEvalOscillation.py
+++
@@ -1,9 +0,0 @@
-from evaluators.EvaluatorAvalanches import EvaluatorAvalanches
-import services.ServiceSavedModel as ssm
-
-filename = "test_stress_9_tmax=500000_1"
-modelParams, simulationData, switchTypeValues = ssm.loadModel(f"{filename}.json", loadSwitchValues=True)
-times, positions, orientations = simulationData
-
-evaluator = EvaluatorAvalanches(orientations=orientations, orderThreshold=0.9, savePath=filename)
-evaluator.evaluateAvalanches()
