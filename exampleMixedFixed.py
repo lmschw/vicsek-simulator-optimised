279ba4dcb0d6feb9d141ee677a74b2f598821f8a
lmschw <lillyschwarzenbach@gmail.com>
2025-04-02 19:01:32 +0200

    add log to k staggering script

--- exampleMixedFixed.py
+++ exampleMixedFixed.py
@@ -31,7 +31,7 @@ degreesOfVision = 2 * np.pi
 tmax = 15000
 iStart = 1
 iStop = 11
-saveLocation = "results_noise_stagger"
+saveLocation = "J:/stagger/"
 
 # TEST VALS
 nsms = [NeighbourSelectionMechanism.ALL,
@@ -54,8 +54,8 @@ nsmCombos = [[NeighbourSelectionMechanism.FARTHEST, NeighbourSelectionMechanism.
 
 kCombos = [[1,5]]
 
-densities = [0.06]
-radii = [10]
+densities = [0.06, 0.05, 0.07]
+radii = [10, 5]
 initialConditions = ["ordered", "random"]
 
 percentageFirstValue = 0.5
@@ -75,6 +75,8 @@ for noisePercentage in noisePercentages:
                 for initialCondition in initialConditions:
                     for i in range(iStart, iStop):
                         
+                        startI = time.time()
+                        ServiceGeneral.logWithTime(f"Starting NOSW, NOEV d={density}, r={radius}, noise={noisePercentage}, nsm={nsm.value}, init={initialCondition}, i={i}")
                         events = []
                         if initialCondition == "ordered":
                             initialState = ServicePreparation.createOrderedInitialDistributionEquidistancedIndividual(None, domainSize, n, angleX=initialAngleX, angleY=initialAngleY)
@@ -102,6 +104,9 @@ for noisePercentage in noisePercentages:
                         savePath = f"{saveLocation}local_nosw_noev_stagger=({percentageFirstValue},{ks},{enforceSplit})_d={density}_r={radius}_{initialCondition}_nsm={nsm.value}_ks={ks[0]}-{ks[1]}_noise={noisePercentage}_{i}.json"
                         ServiceSavedModel.saveModel(simulationData=simulationData, path=savePath, 
                                                     modelParams=simulator.getParameterSummary(), switchValues=switchTypeValues)
+                        endI = time.time()
+                        ServiceGeneral.logWithTime(f"Completed NOSW, NOEV d={density}, r={radius}, noise={noisePercentage}, nsm={nsm.value}, init={initialCondition}, i={i} in {ServiceGeneral.formatTime(endI-startI)}")
+
 
     # ------------------------ NOSW, NOEV ---------------------------------
     enforceSplit = False
@@ -111,7 +116,8 @@ for noisePercentage in noisePercentages:
             for nsm in nsms:
                 for initialCondition in initialConditions:
                     for i in range(iStart, iStop):
-                        
+                        startI = time.time()
+                        ServiceGeneral.logWithTime(f"Starting NOSW, NOEV d={density}, r={radius}, noise={noisePercentage}, nsm={nsm.value}, init={initialCondition}, i={i}")
                         events = []
                         if initialCondition == "ordered":
                             initialState = ServicePreparation.createOrderedInitialDistributionEquidistancedIndividual(None, domainSize, n, angleX=initialAngleX, angleY=initialAngleY)
@@ -139,3 +145,8 @@ for noisePercentage in noisePercentages:
                         savePath = f"{saveLocation}local_nosw_noev_stagger=({percentageFirstValue},{ks},{enforceSplit})_d={density}_r={radius}_{initialCondition}_nsm={nsm.value}_ks={ks[0]}-{ks[1]}_noise={noisePercentage}_{i}.json"
                         ServiceSavedModel.saveModel(simulationData=simulationData, path=savePath, 
                                                     modelParams=simulator.getParameterSummary(), switchValues=switchTypeValues)
+                        endI = time.time()
+                        ServiceGeneral.logWithTime(f"Completed NOSW, NOEV d={density}, r={radius}, noise={noisePercentage}, nsm={nsm.value}, init={initialCondition}, i={i} in {ServiceGeneral.formatTime(endI-startI)}")
+
+endOverall = time.time()
+ServiceGeneral.logWithTime(f"Completed run in {ServiceGeneral.formatTime(endOverall-startOverall)}")
