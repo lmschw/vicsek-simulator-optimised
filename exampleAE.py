95af81432a2cd13c6c6c61acf43c65fa9d254d01
lmschw <lillyschwarzenbach@gmail.com>
2026-01-06 17:34:42 +0100

    update example to include all parameters

--- exampleAE.py
+++ exampleAE.py
@@ -1,11 +1,34 @@
-import model.ActiveElastic as ae
+import numpy as np
 
+import model.ActiveElastic as ae
+from enums.EnumNeighbourSelectionMechanism import NeighbourSelectionMechanism
+
+# environment and simulation parameters
 n_agents = 3
 n_steps = 10000
 env_size = 100
+
+# perception parameters
+degrees_of_vision = 2*np.pi
+radius = np.inf
+
+# neighbour selection parameters
+nsm = NeighbourSelectionMechanism.NEAREST
+k = 1
+
+# visualisation parameters
 graph_freq = 10
 visualize = True
 follow = True
 
-sim = ae.SwarmSimulation(n_agents, n_steps, env_size, visualize, follow, graph_freq)
+sim = ae.SwarmSimulation(num_agents=n_agents, 
+                         num_steps=n_steps, 
+                         env_size=env_size, 
+                         degrees_of_vision=degrees_of_vision,
+                         radius=radius,
+                         neighbour_selection_mechanism=nsm,
+                         k=k,
+                         visualize=visualize, 
+                         follow=follow, 
+                         graph_freq=graph_freq)
 sim.run()
