4371bb51b90f319554cfed8d943a4330cd01b391
lmschw <lillyschwarzenbach@gmail.com>
2026-01-07 15:45:18 +0100

    add iterations to AE runner

--- exampleAE.py
+++ exampleAE.py
@@ -28,26 +28,30 @@ debug_prints = False
 iteration_print_frequency = 500
 
 # saving
-savefile_name=f"ae_{nsm.name}_k={k}_n={n_agents}_steps={n_steps}_dov={np.round(degrees_of_vision,2)}_r={radius}"
 save_frequency=1 
 results_dir = os.path.join(os.path.dirname(__file__), "results")
 os.makedirs(results_dir, exist_ok=True)
 
-sim = ae.SwarmSimulation(num_agents=n_agents, 
-                         num_steps=n_steps, 
-                         env_size=env_size, 
-                         degrees_of_vision=degrees_of_vision,
-                         radius=radius,
-                         neighbour_selection_mechanism=nsm,
-                         k=k,
-                         visualize=visualize, 
-                         follow=follow, 
-                         graph_freq=graph_freq,
-                         debug_prints=debug_prints,
-                         iteration_print_frequency=iteration_print_frequency,
-                         savefile_name=savefile_name,
-                         save_frequency=save_frequency,
-                         results_dir=results_dir)
-sim.run()
+num_iters = 2
 
+for iter in range(1,num_iters+1):
+    savefile_name=f"ae_{nsm.name}_k={k}_n={n_agents}_steps={n_steps}_dov={np.round(degrees_of_vision,2)}_r={radius}_{iter}"
+
+    sim = ae.SwarmSimulation(num_agents=n_agents, 
+                            num_steps=n_steps, 
+                            env_size=env_size, 
+                            degrees_of_vision=degrees_of_vision,
+                            radius=radius,
+                            neighbour_selection_mechanism=nsm,
+                            k=k,
+                            visualize=visualize, 
+                            follow=follow, 
+                            graph_freq=graph_freq,
+                            debug_prints=debug_prints,
+                            iteration_print_frequency=iteration_print_frequency,
+                            savefile_name=savefile_name,
+                            save_frequency=save_frequency,
+                            results_dir=results_dir)
+    sim.run()
+    print(f"Done with {iter}/{num_iters}")
 print("Done")
